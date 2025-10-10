a7e12c1559996ab4fb16b6369b0169f76d2a7827
Federico Rivero <federicorivero002@gmail.com>
2025-10-10 19:05:33 -0300

    recuperar actores

--- db_falsa.js
+++ db_falsa.js
@@ -4,7 +4,16 @@ import { Premio } from "./premio"
 
 export class Dbfalsa{
     peliculas=[new peliculas(1,"300","Accion",[1],[1,2]),new peliculas (2,"metegol","Deportes",[1],[]),new peliculas(3,"after","romance",[2,3],[3])]
-    actores=[new Actores(1,"ricardo","darin","21/4/1968",[2,1]),new Actores(2,"guillermo","francella","09/19/1975",[3]),new Actores(3,"florecia", "peña","14/05/22",[3])
-    ]
+    actores=[new Actores(1,"ricardo","darin","21/4/1968",[2,1]),new Actores(2,"guillermo","francella","09/19/1975",[3]),new Actores(3,"florecia", "peña","14/05/22",[3])]
     premios=[new Premio(1,"oscar","17/10/2021",1),new Premio (2,"martin fierro", "22/8/2003",1), new Premio(3,"globo de oro","04/11/2023", 3)]
+
+    recuperarPelis() {
+        let pelis=this.peliculas
+        pelis=pelis.map((peli)=>{
+            peli.actores=peli.actores.map((id)=>{
+                this.actores.filter(id)
+            })
+        })
+        return pelis
+    }
 }
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const { Dbfalsa } = require('./db_falsa')
 const app = express()
 const port = 3000
+const db=new Dbfalsa()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/pelis', (req, res) => {
+  res.send(db.recuperarPelis())
 })
 
 app.listen(port, () => {
